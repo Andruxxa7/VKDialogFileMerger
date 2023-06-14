76208af873ccbbfc36f8fd248dab7cc511ca5256
Andruxxa7 <andruxakosinec7@gmail.com>
2023-06-14 16:07:28 +0300

    Fix #3 Looks like original dialog, without pagination

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ namespace VKDialogHistoryFileMerger
             using (var writer = new StreamWriter(outputFileName, false, Encoding.GetEncoding(1251)))
             {
                 writer.WriteLine(
-                    "<!DOCTYPE html><html><head><meta charset=\"windows-1251\"><title>VK</title><link rel=\"shortcut icon\" href=\"../../favicon.ico\"><link rel=\"stylesheet\" type=\"text/css\" href=\"../../style.css\"></head><body><div class=\"wrap\"><div class=\"header\"><div class=\"page_header\"><div class=\"top_home_logo\"></div></div></div><div class=\"page_content page_block\">");
+                    "<!DOCTYPE html><html><head><meta charset=\"windows-1251\"><title>VK</title><link rel=\"shortcut icon\" href=\"../../favicon.ico\"><link rel=\"stylesheet\" type=\"text/css\" href=\"../../style.css\"></head><body><div class=\"wrap\"><div class=\"header\"><div class=\"page_header\"><div class=\"top_home_logo\"></div></div></div><div class=\"page_content page_block\"><div class=\"wrap_page_content\">");
                 
                     writer.WriteLine(
                         $"<div class=\"page_block_header clear_fix\"><div class=\"page_block_header_inner _header_inner\"><div class=\"ui_crumb\">{name}</div></div></div>");
@@ -49,10 +49,9 @@ namespace VKDialogHistoryFileMerger
 
                         uniqueMessages.Add(dataId, messageContent);
                     }
-                    writer.WriteLine("</div><div class=\"page_content page_block\">");
                 }
 
-                writer.WriteLine("</div></div></body></html>");
+                writer.WriteLine("</div></div></div></body></html>");
                 writer.Close();
             }
 
