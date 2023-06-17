3e283919bbe997d3c9287c147491b5d403df2d6b
Andruxxa7 <andruxakosinec7@gmail.com>
2023-06-17 15:59:38 +0300

    Create form and implement path buttons on form

---
+++ VkDialogHistoryFileMergerForm/MergeDMForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace VkDialogHistoryFileMergerForm
+{
+    partial class MergeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mergeButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.inputPathLabel = new System.Windows.Forms.Label();
+            this.outputPathLabel = new System.Windows.Forms.Label();
+            this.selectInputButton = new System.Windows.Forms.Button();
+            this.selectOutputButton = new System.Windows.Forms.Button();
+            this.clearOutputButton = new System.Windows.Forms.Button();
+            this.clearInputButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mergeButton
+            // 
+            this.mergeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.mergeButton.Location = new System.Drawing.Point(629, 380);
+            this.mergeButton.Name = "mergeButton";
+            this.mergeButton.Size = new System.Drawing.Size(160, 42);
+            this.mergeButton.TabIndex = 4;
+            this.mergeButton.Text = "Merge";
+            this.mergeButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitButton.Location = new System.Drawing.Point(463, 380);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(160, 42);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // inputPathLabel
+            // 
+            this.inputPathLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputPathLabel.AutoEllipsis = true;
+            this.inputPathLabel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.inputPathLabel.Font = new System.Drawing.Font("Cooper Black", 15.75F, System.Drawing.FontStyle.Italic);
+            this.inputPathLabel.Location = new System.Drawing.Point(220, 45);
+            this.inputPathLabel.Name = "inputPathLabel";
+            this.inputPathLabel.Size = new System.Drawing.Size(541, 143);
+            this.inputPathLabel.TabIndex = 2;
+            this.inputPathLabel.Text = "Input path:";
+            // 
+            // outputPathLabel
+            // 
+            this.outputPathLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputPathLabel.AutoEllipsis = true;
+            this.outputPathLabel.Font = new System.Drawing.Font("Cooper Black", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputPathLabel.Location = new System.Drawing.Point(222, 235);
+            this.outputPathLabel.Name = "outputPathLabel";
+            this.outputPathLabel.Size = new System.Drawing.Size(566, 125);
+            this.outputPathLabel.TabIndex = 3;
+            this.outputPathLabel.Text = "Output path:";
+            // 
+            // selectInputButton
+            // 
+            this.selectInputButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.selectInputButton.Location = new System.Drawing.Point(12, 45);
+            this.selectInputButton.Name = "selectInputButton";
+            this.selectInputButton.Size = new System.Drawing.Size(178, 63);
+            this.selectInputButton.TabIndex = 1;
+            this.selectInputButton.Text = "Select input folder";
+            this.selectInputButton.UseVisualStyleBackColor = true;
+            this.selectInputButton.Click += new System.EventHandler(this.selectInputButton_Click);
+            // 
+            // selectOutputButton
+            // 
+            this.selectOutputButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.selectOutputButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.selectOutputButton.Location = new System.Drawing.Point(12, 226);
+            this.selectOutputButton.Name = "selectOutputButton";
+            this.selectOutputButton.Size = new System.Drawing.Size(178, 63);
+            this.selectOutputButton.TabIndex = 2;
+            this.selectOutputButton.Text = "Select output folder";
+            this.selectOutputButton.UseVisualStyleBackColor = true;
+            this.selectOutputButton.Click += new System.EventHandler(this.selectOutputButton_Click);
+            // 
+            // clearOutputButton
+            // 
+            this.clearOutputButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.clearOutputButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clearOutputButton.Location = new System.Drawing.Point(12, 297);
+            this.clearOutputButton.Name = "clearOutputButton";
+            this.clearOutputButton.Size = new System.Drawing.Size(178, 63);
+            this.clearOutputButton.TabIndex = 3;
+            this.clearOutputButton.Text = "Clear";
+            this.clearOutputButton.UseVisualStyleBackColor = true;
+            this.clearOutputButton.Click += new System.EventHandler(this.clearOutputButton_Click);
+            // 
+            // clearInputButton
+            // 
+            this.clearInputButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clearInputButton.Location = new System.Drawing.Point(12, 125);
+            this.clearInputButton.Name = "clearInputButton";
+            this.clearInputButton.Size = new System.Drawing.Size(178, 63);
+            this.clearInputButton.TabIndex = 6;
+            this.clearInputButton.Text = "Clear";
+            this.clearInputButton.UseVisualStyleBackColor = true;
+            this.clearInputButton.Click += new System.EventHandler(this.clearInputButton_Click);
+            // 
+            // MergeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clearInputButton);
+            this.Controls.Add(this.clearOutputButton);
+            this.Controls.Add(this.selectOutputButton);
+            this.Controls.Add(this.selectInputButton);
+            this.Controls.Add(this.outputPathLabel);
+            this.Controls.Add(this.inputPathLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.mergeButton);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Name = "MergeForm";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button clearInputButton;
+
+        private System.Windows.Forms.Button selectOutputButton;
+        private System.Windows.Forms.Button clearOutputButton;
+
+        private System.Windows.Forms.Button selectInputButton;
+
+        private System.Windows.Forms.Button mergeButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label inputPathLabel;
+        private System.Windows.Forms.Label outputPathLabel;
+
+        #endregion
+    }
+}
---
+++ VkDialogHistoryFileMergerForm/MergeDMForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VkDialogHistoryFileMergerForm
+{
+    public partial class MergeForm : Form
+    {
+        private string _inputPath = ".";
+        private string _outputPath;
+
+        public MergeForm()
+        {
+            FormClosing += (_, _) => Application.Exit();
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e) => Close();
+
+        private void selectInputButton_Click(object sender, EventArgs e)
+        {
+            using var folderDialog = new FolderBrowserDialog();
+            var result = folderDialog.ShowDialog();
+            if (result != DialogResult.OK || string.IsNullOrWhiteSpace(folderDialog.SelectedPath)) return;
+            _inputPath = folderDialog.SelectedPath;
+            inputPathLabel.Text = $"Input path: {_inputPath}";
+        }
+
+        private void selectOutputButton_Click(object sender, EventArgs e)
+        {
+            using var folderDialog = new FolderBrowserDialog();
+            var result = folderDialog.ShowDialog();
+            if (result != DialogResult.OK || string.IsNullOrWhiteSpace(folderDialog.SelectedPath)) return;
+            _outputPath = folderDialog.SelectedPath;
+            outputPathLabel.Text = $"Output path: {_outputPath}";
+        }
+
+        private void clearInputButton_Click(object sender, EventArgs e)
+        {
+            inputPathLabel.Text = "Input path:";
+            _inputPath = ".";
+        }
+
+        private void clearOutputButton_Click(object sender, EventArgs e)
+        {
+            outputPathLabel.Text = "Output path:";
+            _outputPath = null;
+        }
+    }
+}
