766784712b678ca66c57ad378f1a12ed4fdc9ae6
Andruxxa7 <andruxakosinec7@gmail.com>
2023-06-20 14:21:06 +0300

    Add Options for args handling feature

---
+++ Class/Options.cs
@@ -0,0 +1,17 @@
+#nullable enable
+using CommandLine;
+
+namespace VKDialogHistoryFileMerger.Class
+{
+    public class Options
+    {
+        [Option('i', "path", Required = false, HelpText = "Path to the dialog files directory.")]
+        public string Path { get; set; } = ".";
+
+        [Option('c', "addCss", Required = false, HelpText = "Add CSS inside the merged file.")]
+        public bool AddCss { get; set; } = false;
+
+        [Option('o', "outputpath", Required = false, HelpText = "Output path for the merged file.")]
+        public string? OutputPath { get; set; } = null;
+    }
+}
