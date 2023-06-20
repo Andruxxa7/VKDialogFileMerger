c7708b2a54e8a1a68bc15b2924c8f3a8b6820d1d
Andruxxa7 <andruxakosinec7@gmail.com>
2023-06-20 12:17:04 +0300

    Implement css feature to cli version

--- Program.cs
+++ Program.cs
@@ -7,8 +7,11 @@ namespace VKDialogHistoryFileMerger
     {
         private static void Main()
         {
+            Console.WriteLine("Do you want to add css inside merged file or keep link? Y\\N(or other");
+            var addCss = Console.ReadKey().KeyChar == 'Y';
+            Console.WriteLine();
             Console.WriteLine(ExistsDialogFiles().Result
-                ? MergeFiles()
+                ? MergeFiles(addCss)
                 : "There are no files. Move script to dialog directory and try again.");
             Console.ReadKey();
         }
