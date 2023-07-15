19a8e87b090cfaab380dc1143ba8ae3cbd6ee320
Andruxxa7 <andruxakosinec7@gmail.com>
2023-07-15 23:43:00 +0300

    Refactor three identical patterns into a single variable

--- VKDialogFileMergerService/Services/VkDialogHistoryFileMergerService.cs
+++ VKDialogFileMergerService/Services/VkDialogHistoryFileMergerService.cs
@@ -9,14 +9,16 @@ namespace VKDialogHistoryFileMergerService;
 
 public static class VkDialogHistoryFileMergerService
 {
+    private static string _fileNamePattern = @"messages(\d+)\.html";
+
     public static async Task<string> MergeFiles(bool addCss, string? outputpath = null) =>
         await MergeFiles(".", addCss, outputpath);
 
     public static async Task<string> MergeFiles(string path, bool addCss, string? outputpath = null)
     {
         var htmlFiles = Directory.EnumerateFiles(path, "messages*.html")
-            .Where(file => Regex.IsMatch(file, @"messages\d+\.html"))
-            .OrderBy(file => int.Parse(Regex.Match(file, @"messages(\d+)\.html").Groups[1].Value))
+            .Where(file => Regex.IsMatch(file, _fileNamePattern))
+            .OrderBy(file => int.Parse(Regex.Match(file, _fileNamePattern).Groups[1].Value))
             .ToArray();
         var outputFileName = "MergedDialog.html";
         var doc = new HtmlDocument();
@@ -24,7 +26,7 @@ public static class VkDialogHistoryFileMergerService
         doc.LoadHtml(await File.ReadAllTextAsync(htmlFiles[0], Encoding.GetEncoding(1251)));
         var divNode = doc.DocumentNode.SelectSingleNode("//div[@class='ui_crumb']");
         outputFileName = Path.Combine(outputpath ?? string.Empty, divNode?.InnerText + outputFileName);
-        var writer = new StringBuilder(2 + htmlFiles.Length*50);
+        var writer = new StringBuilder(2 + htmlFiles.Length * 50);
         writer.AppendLine(
             $"<!DOCTYPE html><html><head><meta charset=\"windows-1251\"><title>VK</title><link rel=\"shortcut icon\" href=\"../../favicon.ico\">{(addCss ? Resources.css : Resources.cssLink)}</head><body><div class=\"wrap\"><div class=\"header\"><div class=\"page_header\"><div class=\"top_home_logo\"></div></div></div><div class=\"page_content page_block\"><div class=\"wrap_page_content\"><div class=\"page_block_header clear_fix\"><div class=\"page_block_header_inner _header_inner\"><div class=\"ui_crumb\">{divNode?.InnerText}</div></div></div>");
 
@@ -59,7 +61,8 @@ public static class VkDialogHistoryFileMergerService
     {
         try
         {
-            return Task.FromResult(Directory.EnumerateFiles(path, "messages*.html").Any(file => Regex.IsMatch(file, @"messages\d+\.html")));
+            return Task.FromResult(Directory.EnumerateFiles(path, "messages*.html")
+                .Any(file => Regex.IsMatch(file, _fileNamePattern)));
         }
         catch (DirectoryNotFoundException)
         {
